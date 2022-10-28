52a67757321f8da5091245e35afcc83aae7ec23b
Paul Thomas <paulthomas2107@gmail.com>
2022-10-28 18:08:11 +0100

    More....check for potted white

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ FPS = 120
 # Game vars
 dia = 36
 pocket_dia = 66
-taking_shot = True
 force = 0
 max_force = 10000
 force_direction = 1
+taking_shot = True
 powering_up = False
+cue_ball_potted = False
 potted_balls = []
-
 
 # Colours
 BG = (50, 50, 50)
@@ -158,10 +158,16 @@ while run:
             ball_y_dist = abs(ball.body.position[1] - pocket[1])
             ball_dist = math.sqrt((ball_x_dist ** 2) + (ball_y_dist ** 2))
             if ball_dist <= pocket_dia / 2:
-                space.remove(ball.body)
-                balls.remove(ball)
-                potted_balls.append(ball_images[i])
-                ball_images.pop(i)
+                # check for potted white
+                if i == len(balls) - 1:
+                    cue_ball_potted = True
+                    ball.body.position = (-100, -100)
+                    ball.body.velocity = (0.0, 0.0)
+                else:
+                    space.remove(ball.body)
+                    balls.remove(ball)
+                    potted_balls.append(ball_images[i])
+                    ball_images.pop(i)
 
     # Draw pool balls
     for i, ball in enumerate(balls):
@@ -175,6 +181,10 @@ while run:
 
     # Draw cue
     if taking_shot:
+        if cue_ball_potted:
+            # Reposition cue ball
+            balls[-1].body.position = (888, SCREEN_HEIGHT / 2)
+            cue_ball_potted = False
         # Calc cue angle
         mouse_pos = pygame.mouse.get_pos()
         cue.rect.center = balls[-1].body.position
@@ -200,6 +210,9 @@ while run:
         balls[-1].body.apply_impulse_at_local_point((force * -x_impulse, force * y_impulse), (0, 0))
         force = 0
         force_direction = 1
+
+    # Draw bottom panel
+    pygame.draw.rect(screen, BG, (0, SCREEN_HEIGHT, SCREEN_WIDTH, BOTTOM_PANEL))
 
     # Draw potted balls in panel
     for i, ball in enumerate(potted_balls):
